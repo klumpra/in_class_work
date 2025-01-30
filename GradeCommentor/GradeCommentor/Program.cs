e53aa1560409a31f5286f6ba769ba8e98aa7a727
Ray Klump <klumpra@lewisu.edu>
2025-01-29 19:07:30 -0600

    Added an example with a switch statement and then a for loop

---
+++ GradeCommentor/GradeCommentor/Program.cs
@@ -0,0 +1,47 @@
+﻿/* based on the grade the user enters, comment on it. 
+ * Illustrates using switch statements. */
+using static System.Console;
+namespace GradeCommentor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter your letter grade: ");
+            string letterGrade = ReadLine().ToUpper(); // makes what they typed upper case
+            switch (letterGrade)
+            {
+                case "A":
+                    WriteLine("Congratulations! You're pretty smart.");
+                    break;
+                case "B":
+                    WriteLine("Good job. Keep it up.");
+                    break;
+                case "C":
+                    WriteLine("C is for cookie, and cookies are delicious.");
+                    break;
+                case "D":
+                    WriteLine("D is for diploma.");
+                    break;
+                case "F":
+                    WriteLine("Mom and Dad are going to be mad.");
+                    break;
+                default:
+                    WriteLine("You're so dumb.");
+                    break;
+            }
+            /*
+            if (letterGrade == "A")
+            {
+                WriteLine("Smart");
+            } else if (letterGrade == "B")
+            {
+                WriteLine("Good");
+            } else
+            {
+                WriteLine("You're so dumb.");
+            }
+            */
+        }
+    }
+}
---
+++ SumTheNumbers/SumTheNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿/* use a for loop to sum up all the number up to the number entered by the user. */
+using static System.Console;
+namespace SumTheNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cap;
+            Write("Enter the number you want to sum up to ...");
+            cap = int.Parse(ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= cap; i++)
+            {
+                sum = sum + i;
+            }
+            WriteLine($"The sum is {sum}.");
+        }
+    }
+}
