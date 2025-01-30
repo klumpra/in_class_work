df3a043dbb1173d947ddd6ba5a252f5931434e59
Ray Klump <klumpra@lewisu.edu>
2025-01-29 18:42:45 -0600

    Added two even-odd programs.

---
+++ EvenOddUsingInLineConditional/EvenOddUsingInLineConditional/Program.cs
@@ -0,0 +1,19 @@
+﻿/* This program determines if a number entered by the user
+ * is even or odd. It uses an inline conditional to do so.
+ */
+using static System.Console;
+namespace EvenOddUsingInLineConditional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Write("Enter a number: ");
+            number = int.Parse(ReadLine());
+            //           string judgment = number % 2 == 0 ? "even" : "odd";
+            //           WriteLine($"The number {number} is {judgment}.");
+            WriteLine($"The number {number} is {(number % 2 == 0 ? "even" : "odd")}.");
+        }
+    }
+}
---
+++ EvenOddWithIf/EvenOddWithIf/Program.cs
@@ -0,0 +1,29 @@
+﻿/* This simple program asks the user for an integer and then determines
+ * if it is even or odd */
+using static System.Console;
+namespace EvenOddWithIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Write("Enter an integer: ");
+            try
+            {
+                number = int.Parse(ReadLine());
+            } catch (Exception ex)
+            {
+                WriteLine("You had to enter an integer;");
+                return;
+            }
+            if (number % 2  == 0)  // is the remainder when divided by 2 zero?
+            {
+                WriteLine($"The number {number} is even.");
+            } else
+            {
+                WriteLine($"The number {number} is odd.");
+            }
+        }
+    }
+}
