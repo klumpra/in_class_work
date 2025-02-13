3df8ebc15bd6b7a01c7acc855a3aa5eeabaeab41
Ray Klump <klumpra@lewisu.edu>
2025-02-12 19:16:18 -0600

    Circle class and Main

---
+++ FunWithCircles20250212/FunWithCircles20250212/Circle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace FunWithCircles20250212
+{
+    internal class Circle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                if (value < 0)  // value is a reserved word
+                {
+                    radius = 0;
+                }
+                else
+                {
+                    radius = value;
+                }
+            }
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        //default constructor
+        public Circle()
+        {  // in constructors, use the names of your properties
+            X = 0;
+            Y = 0;
+            Radius = 0;
+        }
+
+        //non-default constructor
+        public Circle(double x, double y, double radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public double CalculatePerim()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        // add a ToString function to represent a circle object as a string
+        public override string ToString()
+        {
+            return $"{X,0:F2}\t{Y,0:F2}\t{Radius,0:F2}";
+        }
+    }
+}
---
+++ FunWithCircles20250212/FunWithCircles20250212/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+namespace FunWithCircles20250212
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter x, y, and radius: ");
+            string response = ReadLine();
+            string[] parts = response.Split(" ");
+            double x, y, rad;
+            x = double.Parse(parts[0]);
+            y = double.Parse(parts[1]);
+            rad = double.Parse(parts[2]);
+            Circle c = new Circle(x, y, rad);  // c is a Circle OBJECT
+            double area, circumference;
+            area = c.CalculateArea();
+            circumference = c.CalculatePerim();
+            WriteLine("Here is the circle: ");
+            WriteLine(c);   // c is represented with its circle-related data thanks to ToString
+            WriteLine($"Area = {area,0:F2}, Circumference = {circumference,0:F2}");
+        }
+    }
+}
