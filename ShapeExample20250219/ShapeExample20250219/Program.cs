1a0c803e71b115b5fa405b8775dc9fe96de30a4a
Ray Klump <klumpra@lewisu.edu>
2025-02-19 19:18:47 -0600

    Version with List<Shape> and polymorphism

--- ShapeExample20250219/ShapeExample20250219/Program.cs
+++ ShapeExample20250219/ShapeExample20250219/Program.cs
@@ -3,12 +3,39 @@ namespace ShapeExample20250219
 {
     internal class Program
     {
+        static void PrintShapesToScreen(List<Shape> shapes)
+        {
+            foreach (Shape shape in shapes)
+            {
+                WriteLine(shape); // thanks to polymorphism, the right ToString will be called
+            }
+        }
+        static void PrintAreasAndPerimeters(List<Shape> shapes)
+        {
+            double area, perim;
+            foreach (Shape shape in shapes)
+            {
+                area = shape.CalculateArea();
+                perim = shape.CalculatePerimeter();
+                WriteLine($"Area = {area,0:F2}, Perim = {perim,0:F2}");
+            }
+        }
         static void Main(string[] args)
         {
             Circle defaultCircle = new Circle();
             Circle customCircle = new Circle(7, 3, 5);
             Rectangle defaultRectangle = new Rectangle();
             Rectangle customRectangle = new Rectangle(12, 5, 8, 10);
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(defaultCircle);
+            shapes.Add(customCircle);
+            shapes.Add(customRectangle);
+            shapes.Add(defaultRectangle);
+            // shapes.Add(new Circle(4, 2, 3));
+            PrintShapesToScreen(shapes);
+            PrintAreasAndPerimeters(shapes);
+
+            /*
             WriteLine("Here are your circles:");
             WriteLine(defaultCircle);
             WriteLine(customCircle);
@@ -17,7 +44,7 @@ namespace ShapeExample20250219
             WriteLine(defaultRectangle);
             WriteLine(customRectangle);
             WriteLine($"The area of the rectangle is {customRectangle.CalculateArea(),0:F2}, and the perimeter is {customRectangle.CalculatePerimeter(),0:F2}.");
-
+            */
         }
     }
 }
